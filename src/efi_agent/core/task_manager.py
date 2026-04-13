AV-EFI/efi-agent

commit 4385d904cea0fe6af49df7c1f4c3876d6c4caabf

epic_client: Deal with strange case of PIDs not found

src/efi_agent/core/task_manager.py
@@ -258,11 +258,21 @@ class Task:
             if not handler.record \
                or isinstance(handler.record, efi.Manifestation):
                 if not handler.tasks.get(Operation.CREATE):
-                    r = self.client.get(handler.pid)
-                    # Note that old record potentially does not
-                    # validate against current schema yet
-                    pid, json_record = self.client.efi_from_response(
-                        r, unvalidated_json=True)
+                    try:
+                        r = self.client.get(handler.pid)
+                    except httpx.HTTPStatusError as e:
+                        if e.response.status_code == 404 and handler.record:
+                            log.warning(
+                                f"PID '{handler.pid}' did not exist when"
+                                f" trying to update, created it instead")
+                            pass
+                        else:
+                            raise
+                    else:
+                        # Note that old record potentially does not
+                        # validate against current schema yet
+                        pid, json_record = self.client.efi_from_response(
+                            r, unvalidated_json=True)
                     # Todo: Update references if PID has become an alias
                     if pid != handler.pid:
                         raise RuntimeError(

commit da669adf6b22d6d122ef5dbc7c6b47ca60376ee9

Fix bug introduced in commit 4385d90

src/efi_agent/core/task_manager.py
@@ -7,6 +7,7 @@ import logging
 import pathlib
 
 from avefi_schema import model_pydantic_v2 as efi
+import httpx
 from pydantic import ValidationError
 
 