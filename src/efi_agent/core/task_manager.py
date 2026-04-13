AV-EFI/efi-agent

commit 4385d904cea0fe6af49df7c1f4c3876d6c4caabf

epic_client: Deal with strange case of PIDs not found

src/efi_agent/core/task_manager.py
@@ -258,11 +258,21 @@ class Task:
             if not handler.record \
                or isinstance(handler.record, efi.Manifestation):
                 if not handler.tasks.get(Operation.CREATE):
-                    r = self.client.get(handler.pid)
-                    # Note that old record potentially does not
-                    # validate against current schema yet
-                    pid, json_record = self.client.efi_from_response(
-                        r, unvalidated_json=True)
+                    try:
+                        r = self.client.get(handler.pid)
+                    except httpx.HTTPStatusError as e:
+                        if e.response.status_code == 404 and handler.record:
+                            log.warning(
+                                f"PID '{handler.pid}' did not exist when"
+                                f" trying to update, created it instead")
+                            pass
+                        else:
+                            raise
+                    else:
+                        # Note that old record potentially does not
+                        # validate against current schema yet
+                        pid, json_record = self.client.efi_from_response(
+                            r, unvalidated_json=True)
                     # Todo: Update references if PID has become an alias
                     if pid != handler.pid:
                         raise RuntimeError(

commit 009e566850c402f63134afc774e4077bf591588f

Fix another bug introduced in commit 4385d90

src/efi_agent/core/task_manager.py
@@ -258,6 +258,7 @@ class Task:
             # must be an update then
             if not handler.record \
                or isinstance(handler.record, efi.Manifestation):
+                has_item = []
                 if not handler.tasks.get(Operation.CREATE):
                     try:
                         r = self.client.get(handler.pid)
@@ -274,22 +275,20 @@ class Task:
                         # validate against current schema yet
                         pid, json_record = self.client.efi_from_response(
                             r, unvalidated_json=True)
-                    # Todo: Update references if PID has become an alias
-                    if pid != handler.pid:
-                        raise RuntimeError(
-                            f"PID for manifestation changed from {handler.pid}"
-                            f" to {pid}")
-                    if not handler.record:
-                        handler.record = \
-                            efi.MovingImageRecordTypeAdapter.validate(
-                                json_record)
-                        has_item = handler.record.has_item
-                    else:
-                        has_item = [
-                            efi.AVefiResource.model_validate(i)
-                            for i in json_record.get('has_item', [])]
-                else:
-                    has_item = []
+                        # Todo: Update references if PID has become an alias
+                        if pid != handler.pid:
+                            raise RuntimeError(
+                                f"PID for manifestation changed from {handler.pid}"
+                                f" to {pid}")
+                        if not handler.record:
+                            handler.record = \
+                                efi.MovingImageRecordTypeAdapter.validate(
+                                    json_record)
+                            has_item = handler.record.has_item
+                        else:
+                            has_item = [
+                                efi.AVefiResource.model_validate(i)
+                                for i in json_record.get('has_item', [])]
                 for item_handler in _filter_by(
                         handler.referenced_by, 'is_item_of'):
                     item_id = efi.AVefiResource(id=item_handler.pid)